6277642d2e00b5f42ab7e9f8d7496a8fbd332748
Pavel Khon <va7mont@gmail.com>
2022-11-23 12:48:05 +0500

    Products added

---
+++ src/components/Product/Product.jsx
@@ -0,0 +1,29 @@
+import Button from '../UI/Button / Button'
+import './Product.css'
+
+export default function Product(props) {
+
+	const onAddHandler = () => {
+		props.onAdd(props.product)
+	}
+
+	return (
+		<div className={`product ${props.className}`}>
+			<div className={'img'} />
+			<div className={'title'}>
+				{props.product.title}
+			</div>
+			<div className={'description'}>
+				{props.product.description}
+			</div>
+			<div className={'price'}>
+				<span>
+					Стоимость: <b>{props.product.price}</b>
+				</span>
+			</div>
+			<Button className={'add-btn'} onClick={onAddHandler}>
+				Добавить в корзину
+			</Button>
+		</div>
+	)
+}
