f542ebde3f9d36f22ebaf4356f02b502ac22c9bf
Pavel Khon <va7mont@gmail.com>
2022-11-23 12:49:44 +0500

    Tg hook fixed

--- src/hooks/useTelegram.js
+++ src/hooks/useTelegram.js
@@ -7,15 +7,18 @@ export function useTelegram() {
 	}
 
 	const onToggleButton = () => {
-		if (tg.MainButton.isVisible) tg.MainButton.hide()
-		else tg.MainButton.show()
+		if (tg.MainButton.isVisible) {
+			tg.MainButton.hide()
+		} else {
+			tg.MainButton.show()
+		}
 	}
 
 	return {
-		tg,
-		user: tg.initDataUnsafe?.user,
 		onClose,
 		onToggleButton,
+		tg,
+		user: tg.initDataUnsafe?.user,
+		queryId: tg.initDataUnsafe?.query_id,
 	}
-
 }
